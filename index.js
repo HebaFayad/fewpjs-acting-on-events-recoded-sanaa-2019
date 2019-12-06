f7807ffd3454acf2a1b2c5521e7d594bce73e192
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-06 13:06:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function moveDodgerLeft() {
   }
 function moveDodgerRight() {
     let rightNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(rightNumbers, 10);
+    let right = parseInt(rightNumbers, 10);
    
-    if (left < 360 ) {
-      dodger.style.left = `${left + 1}px`;
+    if (right < 360 ) {
+      dodger.style.left = `${right + 1}px`;
     }
   }
 document.addEventListener("keydown", function(e) {
